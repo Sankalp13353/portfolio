4f89e25f1233cb5fb915af121e812788fd5a2e73
Sankalp <sankalp13353@gmail.com>
2026-04-05 21:06:42 +0530

    fixed repo structure

---
+++ portfolio/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} Sankalp. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
